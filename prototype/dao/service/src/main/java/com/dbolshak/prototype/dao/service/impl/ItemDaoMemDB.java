08ac51202d5ed2ce71288974b9b889e908569287
Bolshakov, Denis <bolshakov.denis@gmail.com>
2014-05-08 10:49:15 +0400

    moving services to separate module

---
+++ prototype/dao/service/src/main/java/com/dbolshak/prototype/dao/service/impl/ItemDaoMemDB.java
@@ -0,0 +1,52 @@
+package com.dbolshak.prototype.dao.service.impl;
+
+import com.dbolshak.prototype.dao.model.domain.Item;
+import com.dbolshak.prototype.dao.service.ItemDao;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Simple implementation of ItemDao based on memory.
+ */
+class ItemDaoMemDB implements ItemDao {
+    private final Map<Long, Item> storage = new HashMap<Long, Item>();
+    private final AtomicLong counter = new AtomicLong(0);
+
+    @Override
+    public Item create(String content) {
+        long id = counter.incrementAndGet();
+        Item item = new Item(id, content);
+        storage.put(id, item);
+        return item;
+    }
+
+    @Override
+    public Item read(long id) {
+        return storage.get(id);
+    }
+
+    @Override
+    public boolean update(Item item) {
+        if (has(item.getId())) {
+            storage.put(item.getId(), item);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(long id) {
+        if (has(id)) {
+            storage.remove(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean has(long id) {
+        return storage.containsKey(id);
+    }
+}
---
+++ prototype/dao/service/src/test/java/com/dbolshak/prototype/dao/service/impl/ItemDaoMemDBTest.java
@@ -0,0 +1,72 @@
+package com.dbolshak.prototype.dao.service.impl;
+
+import com.dbolshak.prototype.dao.model.domain.Item;
+import com.dbolshak.prototype.dao.service.ItemDao;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * unit tests for ItemDaoMemDB
+ */
+public class ItemDaoMemDBTest {
+    private ItemDao itemDao;
+    private static final String CONTENT = "Test";
+
+    @Before
+    public void setUp() {
+        itemDao = new ItemDaoMemDB();
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        Item item = itemDao.create(CONTENT);
+        long itemId = item.getId();
+        Assert.assertEquals(CONTENT, itemDao.read(itemId).getContent());
+
+        long anotherItemId = itemDao.create(CONTENT).getId();
+        Assert.assertEquals(CONTENT, itemDao.read(anotherItemId).getContent());
+
+        Assert.assertTrue(anotherItemId != itemId);
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        Item item = itemDao.create(CONTENT);
+        long itemId = item.getId();
+
+        String newContent = "another test";
+        item.setContent(newContent);
+        Assert.assertEquals(true, itemDao.update(item));
+        Assert.assertEquals(newContent, itemDao.read(itemId).getContent());
+
+        item = itemDao.create(CONTENT);
+        item.setId(Long.MAX_VALUE);
+        Assert.assertEquals(false, itemDao.update(item));
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        long itemId = itemDao.create(CONTENT).getId();
+        Assert.assertEquals(itemId, itemDao.read(itemId).getId());
+        Assert.assertTrue(itemDao.read(itemId) != null);
+
+        Assert.assertEquals(true, itemDao.delete(itemId));
+        Assert.assertTrue(itemDao.read(itemId) == null);
+
+        Assert.assertEquals(false, itemDao.delete(Long.MAX_VALUE));
+        Assert.assertTrue(itemDao.read(Long.MAX_VALUE) == null);
+    }
+
+    @Test
+    public void has() {
+        Assert.assertEquals(false, itemDao.has(0));
+        Assert.assertEquals(false, itemDao.has(-1));
+        Assert.assertEquals(false, itemDao.has(1));
+        Assert.assertEquals(false, itemDao.has(Long.MAX_VALUE));
+        Assert.assertEquals(false, itemDao.has(Long.MIN_VALUE));
+
+        long itemId = itemDao.create(CONTENT).getId();
+        Assert.assertEquals(true, itemDao.has(itemId));
+    }
+}
